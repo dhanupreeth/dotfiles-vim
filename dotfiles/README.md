148d7da21b67e91409dd9e9ffe6ee571fcfb4609
dhanesh <dhanupreeth@gmail.com>
2016-10-04 18:21:16 +0530

    added dotfiles

---
+++ dotfiles/README.md
@@ -0,0 +1,3 @@
+# Dotfiles
+
+Some dotfiles for vim and a simple script to symlink in place
---
+++ dotfiles/install.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+cd $HOME/.dotfiles && git pull && cd
+
+files=`find ./.dotfiles/linked -type f | cut -d '/' -f 4-`
+for file in $files; do
+  if [ -f $HOME/\.$file ]; then
+    mv $HOME/\.$file $HOME/\."$file"_bak
+  fi
+  if [ -L $HOME/\.$file ]; then
+    rm $HOME/\.$file
+  fi
+  ln -s $HOME/.dotfiles/linked/$file $HOME/\.$file
+done
+
+#rm -rf $HOME/.vim/bundle
+mkdir -p $HOME/.vim/bundle
+
+if ! test -d $HOME/.vim/bundle/vundle
+then
+  git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+else
+  cd $HOME/.vim/bundle/vundle && git pull && cd
+fi
+vim -u $HOME/.vimrc.bundles +BundleInstall +qa
+
+if test -f $HOME/.bashrc
+then
+  PROFILE_FILE="$HOME/.bashrc"
+else
+  PROFILE_FILE="$HOME/.bash_profile"
+fi
+
+if ! grep -q "alias tmux=\"TERM=screen-256color-bce tmux\"" $PROFILE_FILE
+then
+  echo "alias tmux=\"TERM=screen-256color-bce tmux\"" >> $PROFILE_FILE
+fi
+
+if ! grep -q "export PS1=\"\\\u@\\\h:\\\w\\$ \"" $PROFILE_FILE
+then
+  echo "export PS1=\"\\u@\\h:\\w\$ \"" >> $PROFILE_FILE
+fi
+
+if ! grep -q "export HISTSIZE=10000" $PROFILE_FILE
+then
+  echo "export HISTSIZE=10000" >> $PROFILE_FILE
+fi
+
+if ! grep -q "export HISTFILESIZE=10000" $PROFILE_FILE
+then
+  echo "export HISTFILESIZE=10000" >> $PROFILE_FILE
+fi
+
+if ! grep -q "export HISTTIMEFORMAT='%F %T '" $PROFILE_FILE
+then
+  echo "export HISTTIMEFORMAT='%F %T '" >> $PROFILE_FILE
+fi
+
+if ! grep -q "export PROMPT_COMMAND='history -a'" $PROFILE_FILE
+then
+  echo "export PROMPT_COMMAND='history -a'" >> $PROFILE_FILE
+fi
+
+if ! grep -q "export EDITOR=\$(which vim)" $PROFILE_FILE
+then
+  echo "export EDITOR=\$(which vim)" >> $PROFILE_FILE
+fi
+
+source $PROFILE_FILE
